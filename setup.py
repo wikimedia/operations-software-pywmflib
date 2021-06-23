704f35d9500556257ce36bcd393ddd0f4047a986
Riccardo Coccioli <rcoccioli@wikimedia.org>
2021-06-23 09:59:01 +0200

    Add official support for Python 3.9
    Change-Id: I40eeadee17f1d1385583b39303f566fee8e88029

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Systems Administration',
