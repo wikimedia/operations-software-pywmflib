522b751c906f123eed0784d5e51ba2fc5471c396
Riccardo Coccioli <rcoccioli@wikimedia.org>
2021-11-10 14:12:24 +0100

    constants: add new drmrs datacenter
    Bug: T282787
    Change-Id: I261b049e509b04a17bed06e13043af70ac9a3f7e

--- wmflib/constants.py
+++ wmflib/constants.py
@@ -1,6 +1,6 @@
 """Constants module."""
 
-ALL_DATACENTERS = ('eqiad', 'codfw', 'esams', 'ulsfo', 'eqsin')
+ALL_DATACENTERS = ('eqiad', 'codfw', 'esams', 'ulsfo', 'eqsin', 'drmrs')
 """tuple: list of all datacenters."""
 
 PUBLIC_AUTHDNS = ('208.80.154.238', '208.80.153.231', '91.198.174.239')
@@ -12,5 +12,6 @@ DATACENTER_NUMBERING_PREFIX = {
     'esams': '3',
     'ulsfo': '4',
     'eqsin': '5',
+    'drmrs': '6',
 }
 """dict: prefix for numbered servers in each datacenter."""
