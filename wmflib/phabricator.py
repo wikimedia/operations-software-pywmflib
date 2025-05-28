b2a05150fbe6a419224e570b2c70ff279fa411a2
Riccardo Coccioli <rcoccioli@wikimedia.org>
2025-05-28 14:21:08 +0200

    phabricator: expand support for Phabricator tasks
    * Add a `validate_task_id()` function to validate the format of a
      task ID. Optionally allow empty strings as valid based on the
      `allow_empty_identifiers` argument so that clients can decide if
      a CLI argument that is optional with a default value of empty string
      is considered valid.
    * Add the `allow_empty_identifiers` also to the Phabricator class's
      __init__() and the `create_phabricator()` helper function and support
      for it throughout the class.
    * Add a `task_accessible()` method to verify if a task ID is accessible
      with the given token. It return False if the task doesn't exists or is
      not visible.
    * Add to the new methods and the existing `task_comment()` a `raises`
      parameter that defaults to True (to keep backward compatibility) that
      when set to False allows to call those methods in a best-effort way,
      just logging errors in case of failures and not raising exceptions.
    
    Change-Id: Idfa60c8e6b016171ff65217882ee29db7533a020

--- wmflib/phabricator.py
+++ wmflib/phabricator.py
@@ -2,6 +2,7 @@
 
 import configparser
 import logging
+import re
 
 import phabricator
 
@@ -11,17 +12,23 @@ logger = logging.getLogger(__name__)
 
 
 def create_phabricator(
-    bot_config_file: str, *, section: str = "phabricator_bot", dry_run: bool = True
+    bot_config_file: str,
+    *,
+    section: str = "phabricator_bot",
+    allow_empty_identifiers: bool = False,
+    dry_run: bool = True,
 ) -> phabricator.Phabricator:
     """Initialize the Phabricator client from the bot config file.
 
     Examples:
         ::
 
-            from wmflib.phabricator import create_phabricator
-
-            phab_client = create_phabricator("/path/to/config.ini", dry_run=False)
-            phab_client.task_comment("T12345", "Message")
+            >>> from wmflib.phabricator import create_phabricator, Phabricator
+            >>> phab_client = create_phabricator("/path/to/config.ini", dry_run=False)
+            >>> Phabricator.validate_task_id("T12345")
+            'T12345'
+            >>> if phab_client.task_accessible("T12345"):
+            ...     phab_client.task_comment("T12345", "Message")
 
     Arguments:
         bot_config_file (str): the path to the configuration file for the Phabricator bot, with the following
@@ -34,6 +41,8 @@ def create_phabricator(
 
         section (str, optional): the name of the section of the configuration file where to find the required
             parameters.
+        allow_empty_identifiers: if set to :py:data:`True` all the methods that require an ID (e.g. a task ID) will
+            also accept an empty string as identifier and act as noop in that case.
         dry_run (bool, optional): whether this is a DRY-RUN.
 
     Returns:
@@ -65,7 +74,50 @@ def create_phabricator(
     except Exception as e:
         raise PhabricatorError("Unable to instantiate Phabricator client") from e
 
-    return Phabricator(client, dry_run=dry_run)
+    return Phabricator(client, allow_empty_identifiers=allow_empty_identifiers, dry_run=dry_run)
+
+
+def validate_task_id(task_id: str, *, allow_empty_identifiers: bool = False) -> str:
+    r"""Phabricator task ID validator suitable to be used with :py:meth:`argparse.ArgumentParser.add_argument`.
+
+    Ensures that the task ID is properly formatted as T123456. Empty string are also accepted when setting
+    ``allow_empty_identifiers=True``, suitable to be used with argparse when the argument is optional with
+    a default value of empty string.
+
+    Examples:
+    ::
+
+        >>> validate_task_id("T1")
+        'T1'
+        >>> validate_task_id("T999999")
+        'T999999'
+        >>> validate_task_id("", allow_empty_identifiers=True)
+        ''
+        >>> validate_task_id("T1234567")
+        Traceback (most recent call last):
+          [...SNIP...]
+        ValueError: Invalid Phabricator task ID, expected to match pattern 'T\d{1,6}$', got 'T1234567'
+
+    Arguments:
+        task_id: the Phabricator task ID to validate.
+        allow_empty_identifiers: if set to :py:data:`True` will consider an empty task as valid.
+
+    Raises:
+        ValueError: if the ``task_id`` is not properly formatted, so that argparse will properly format the error
+            message. See also the :py:meth:`argparse.ArgumentParser.add_argument` documentation.
+
+    Returns:
+        the task ID if valid.
+
+    """
+    if allow_empty_identifiers and not task_id:  # Accept empty strings
+        return task_id
+
+    pattern = r"T\d{1,6}$"
+    if re.match(pattern, task_id) is None:
+        raise ValueError(f"Invalid Phabricator task ID, expected to match pattern '{pattern}', got '{task_id}'")
+
+    return task_id
 
 
 class PhabricatorError(WmflibError):
@@ -75,35 +127,150 @@ class PhabricatorError(WmflibError):
 class Phabricator:
     """Class to interact with a Phabricator website."""
 
-    def __init__(self, phabricator_client: phabricator.Phabricator, *, dry_run: bool = True) -> None:
+    def __init__(
+        self,
+        phabricator_client: phabricator.Phabricator,
+        *,
+        dry_run: bool = True,
+        allow_empty_identifiers: bool = False,
+    ) -> None:
         """Initialize the Phabricator client from the bot config file.
 
         Arguments:
             phabricator_client (phabricator.Phabricator): a Phabricator client instance.
             dry_run (bool, optional): whether this is a DRY-RUN.
+            allow_empty_identifiers: if set to :py:data:`True` all the methods that require an ID (e.g. a task ID) will
+                also accept an empty string as identifier and act as noop in that case.
 
         """
         self._client = phabricator_client
         self._dry_run = dry_run
+        self._allow_empty_identifiers = allow_empty_identifiers
 
-    def task_comment(self, task_id: str, comment: str) -> None:
-        """Add a comment on a Phabricator task.
+    def task_accessible(self, task_id: str, *, raises: bool = True) -> bool:
+        """Check if a task exists and is accessible to the curirent API user.
+
+        Examples:
+        ::
+
+            >>> phab_client.task_accessible("T1")
+            True
+            >>> phab_client.task_accessible("T1", raises=False)
+            True
+            >>> phab_client.task_accessible("T999999")
+            False
+            >>> phab_client.task_accessible("")  # with allow_empty_identifiers=True
+            DEBUG:wmflib.phabricator:No task specified and allow_empty_identifiers=True, nothing to check
+            False
+            >>> phab_client.task_accessible("", raises=False)  # with allow_empty_identifiers=True
+            DEBUG:wmflib.phabricator:No task specified and allow_empty_identifiers=True, nothing to check
+            False
+            >>> phab_client.task_accessible("", raises=False)  # with allow_empty_identifiers=False
+            ERROR:wmflib.phabricator:Unable to determine if the task '' is accessible (raises=False): Empty task ID
+            False
+            >>> phab_client.task_accessible("")  # with allow_empty_identifiers=False
+            Traceback (most recent call last):
+              [...SNIP...]
+            wmflib.phabricator.PhabricatorError: Empty task ID
+              [...SNIP...]
+            wmflib.phabricator.PhabricatorError: Unable to determine if the task '' is accessible
+            >>> phab_client.task_accessible("T1")
+            Traceback (most recent call last):
+              [...SNIP...]
+            phabricator.APIError: ERR-INVALID-AUTH: API token "api-a" has the wrong length. [...SNIP...]
+              [...SNIP...]
+            wmflib.phabricator.PhabricatorError: Unable to determine if the task 'T1' is accessible
+            >>> phab_client.task_accessible("T1", raises=False)
+            ERROR:wmflib.phabricator:Unable to determine if the task T1 is accessible (raises=False): ERR-[...SNIP...]
+            ERROR:wmflib.phabricator:Unable to determine if the task 'T1' is accessible (raises=False): ERR[...SNIP...]
+            False
 
         Arguments:
-            task_id (str): the Phabricator task ID (e.g. ``T12345``) to be updated.
-            comment (str): the message to add to the task.
+            task_id: the Phabricator task ID (e.g. ``T12345``) to be updated. If empty string it will raise an
+                exception unless ``allow_empty_identifiers`` is set to :py:data:`True`. In this case it returns
+                :py:data:`False` and logs a message at debug level.
+            raises: if set to :py:data:`False` does not raise an exception and returns :py:data:`False` if unable
+                to check the task accessibility.
 
         Raises:
-            wmflib.phabricator.PhabricatorError: if unable to update the task.
+            wmflib.phabricator.PhabricatorError: if unable to verify if the task is accessible and ``raises`` is
+                set to :py:data:`True`.
+
+        Returns:
+            :py:data:`True` if the task exists and is accessible by the current user, :py:data:`False` if not
+            accessible, an empty string was passed and allow_empty_identifiers is set to :py:data:`True` or if unable
+            to verify if the task is accessible and ``raises`` is set to :py:data:`False`.
 
         """
+        if self._allow_empty_identifiers and not task_id:
+            logger.debug("No task specified and allow_empty_identifiers=True, nothing to check")
+            return False
+
+        try:
+            if not task_id:
+                raise PhabricatorError("Empty task ID")  # noqa: TRY301
+
+            results = self._client.maniphest.search(constraints={"ids": [int(task_id[1:])]})
+            return len(results.data) == 1
+        except Exception as e:
+            message = f"Unable to determine if the task '{task_id}' is accessible"
+            if raises:
+                raise PhabricatorError(message) from e
+
+            logger.error("%s (raises=%s): %s", message, raises, e)
+            return False
+
+    def task_comment(self, task_id: str, comment: str, *, raises: bool = True) -> None:
+        """Add a comment on a Phabricator task.
+
+        Examples:
+        ::
+
+            >>> phab_client.task_comment("T12345", "Message")
+            >>> phab_client.task_comment("", "Message")  # with allow_empty_identifiers=True
+            DEBUG:wmflib.phabricator:No task specified, skipping task update with comment: Message
+            >>> phab_client.task_comment("", "Message", raises=False)  # with allow_empty_identifiers=True
+            DEBUG:wmflib.phabricator:No task specified, skipping task update with comment: Message
+            >>> phab_client.task_comment("T999999", "Message")
+            Traceback (most recent call last):
+              [...SNIP...]
+            phabricator.APIError: ERR-CONDUIT-CORE: Monogram "T999999" does not identify a valid object.
+              [...SNIP...]
+            wmflib.phabricator.PhabricatorError: Unable to update Phabricator task 'T999999'
+            >>> phab_client.task_comment("T999999", "Test comment", raises=False)
+            ERROR:wmflib.phabricator:Unable to update Phabricator task 'T999999' (raises=False): ERR-CONDUI[...SNIP...]
+            >>> phab_client.task_comment("T12345", "Message")  # with dry_run=True
+            DEBUG:wmflib.phabricator:Skip updating Phabricator task 'T12345' in DRY-RUN with comment: Message
+
+        Arguments:
+            task_id (str): the Phabricator task ID (e.g. ``T12345``) to be updated. If empty string no update will be
+                sent and a debug log message will be logged.
+            comment (str): the message to add to the task.
+            raises (bool): if set to :py:data:`False` does not raise an exception if unable to comment on the task.
+
+        Raises:
+            wmflib.phabricator.PhabricatorError: if unable to update the task and ``raises`` is set to
+                :py:data:`True`.
+
+        """
+        if self._allow_empty_identifiers and not task_id:
+            logger.debug("No task specified, skipping task update with comment: %s", comment)
+            return
+
         if self._dry_run:
-            logger.debug("Skip updating Phabricator task %s in DRY-RUN with comment: %s", task_id, comment)
+            logger.debug("Skip updating Phabricator task '%s' in DRY-RUN with comment: %s", task_id, comment)
             return
 
         try:
+            if not task_id:
+                raise PhabricatorError("Empty task ID")  # noqa: TRY301
+
             transactions = [{"type": "comment", "value": comment}]
             self._client.maniphest.edit(objectIdentifier=task_id, transactions=transactions)
             logger.info("Updated Phabricator task %s", task_id)
         except Exception as e:
-            raise PhabricatorError(f"Unable to update Phabricator task {task_id}") from e
+            message = f"Unable to update Phabricator task '{task_id}'"
+            if raises:
+                raise PhabricatorError(message) from e
+
+            logger.error("%s (raises=False): %s", message, e)
--- wmflib/tests/unit/test_phabricator.py
+++ wmflib/tests/unit/test_phabricator.py
@@ -1,16 +1,21 @@
 """Phabricator module tests."""
 
+import logging
+import re
 from unittest import mock
 
 import pytest
 
 from wmflib import phabricator
-from wmflib.tests import get_fixture_path
+from wmflib.tests import check_logs, get_fixture_path
 
 
-def test_create_phabricator_ok():
+@pytest.mark.parametrize("allow_empty_identifiers", (True, False))
+def test_create_phabricator_ok(allow_empty_identifiers):
     """It should initialize the instance."""
-    phab = phabricator.create_phabricator(get_fixture_path("phabricator", "valid.conf"))
+    phab = phabricator.create_phabricator(
+        get_fixture_path("phabricator", "valid.conf"), allow_empty_identifiers=allow_empty_identifiers
+    )
     assert isinstance(phab, phabricator.Phabricator)
 
 
@@ -38,6 +43,50 @@ def test_init_client_raise(mocked_phabricator):
     )
 
 
+@pytest.mark.parametrize("allow_empty_identifiers", (True, False))
+@pytest.mark.parametrize(
+    "task_id",
+    (
+        "",
+        "T1",
+        "T12",
+        "T123",
+        "T1234",
+        "T12345",
+        "T999999",
+    ),
+)
+def test_validate_task_id_ok(task_id, allow_empty_identifiers):
+    """It should not raise if the task_id is empty or valid."""
+    if not task_id and not allow_empty_identifiers:
+        pytest.skip("Skipping ok test for empty string with allow_empty_identifiers=False")
+
+    assert phabricator.validate_task_id(task_id, allow_empty_identifiers=allow_empty_identifiers) == task_id
+
+
+@pytest.mark.parametrize("allow_empty_identifiers", (True, False))
+@pytest.mark.parametrize(
+    "task_id",
+    (
+        "",
+        "-",
+        "T1a",
+        "123",
+        "T1234567",
+    ),
+)
+def test_validate_task_id_fail(task_id, allow_empty_identifiers):
+    """It should raise a ValueError if the task_id is invalid."""
+    if not task_id and allow_empty_identifiers:
+        pytest.skip("Skipping fail test for empty string with allow_empty_identifiers=True")
+
+    with pytest.raises(
+        ValueError,
+        match=re.escape(rf"Invalid Phabricator task ID, expected to match pattern 'T\d{{1,6}}$', got '{task_id}'"),
+    ):
+        phabricator.validate_task_id(task_id)
+
+
 class TestPhabricator:
     """Test class for the Phabricator class."""
 
@@ -47,27 +96,114 @@ class TestPhabricator:
         # pylint: disable=attribute-defined-outside-init
         self.mocked_phabricator_client = mocked_phabricator()
         self.phab = phabricator.Phabricator(self.mocked_phabricator_client, dry_run=False)
+        self.phab_allow_empty = phabricator.Phabricator(
+            self.mocked_phabricator_client, dry_run=False, allow_empty_identifiers=True
+        )
         self.task_comment_transactions = [{"type": "comment", "value": "new message"}]
 
-    def test_task_comment_ok(self):
+    @pytest.mark.parametrize("raises", (True, False))
+    @pytest.mark.parametrize(
+        "data, expected",
+        (
+            ([], False),
+            ([{"id": 12345, "type": "TASK"}], True),
+            ([{"id": 12345, "type": "TASK"}, {"id": 123456, "type": "TASK"}], False),
+        ),
+    )
+    def test_task_accessible_ok(self, data, expected, raises):
+        """It should return True if a task on Phabricator is accessible given the current token."""
+        mocked_results = mock.MagicMock(phabricator.phabricator.Result)
+        mocked_results.data = data
+        self.mocked_phabricator_client.maniphest.search.return_value = mocked_results
+        assert self.phab.task_accessible("T12345", raises=raises) is expected
+        self.mocked_phabricator_client.maniphest.search.assert_called_once_with(constraints={"ids": [12345]})
+
+    @pytest.mark.parametrize("raises", (True, False))
+    def test_task_accessible_empty_ok(self, raises):
+        """It should return False if the task ID is empty and allow_empty_identifiers=True."""
+        assert not self.phab_allow_empty.task_accessible("", raises=raises)
+        self.mocked_phabricator_client.maniphest.search.assert_not_called()
+
+    @pytest.mark.parametrize("task_id", ("", "T12345"))
+    def test_task_accessible_fail_raise(self, task_id):
+        """It should raise a PhabricatorError if unable to verify if the task is accessible."""
+        self.mocked_phabricator_client.maniphest.search.side_effect = RuntimeError("Error")
+        with pytest.raises(
+            phabricator.PhabricatorError, match=f"Unable to determine if the task '{task_id}' is accessible"
+        ):
+            self.phab.task_accessible(task_id)
+
+        if task_id:
+            self.mocked_phabricator_client.maniphest.search.assert_called_once_with(constraints={"ids": [12345]})
+        else:
+            self.mocked_phabricator_client.maniphest.search.assert_not_called()
+
+    @pytest.mark.parametrize("task_id", ("", "T12345"))
+    def test_task_accessible_fail_no_raise(self, task_id, caplog):
+        """It should log an error message if unable to verify if the task is accessible if raises=False."""
+        self.mocked_phabricator_client.maniphest.search.side_effect = RuntimeError("Error")
+        with caplog.at_level(logging.ERROR):
+            assert not self.phab.task_accessible(task_id, raises=False)
+
+        if task_id:
+            self.mocked_phabricator_client.maniphest.search.assert_called_once_with(constraints={"ids": [12345]})
+            message = "Error"
+        else:
+            self.mocked_phabricator_client.maniphest.search.assert_not_called()
+            message = "Empty task ID"
+
+        check_logs(
+            caplog,
+            f"Unable to determine if the task '{task_id}' is accessible (raises=False): {message}",
+            logging.ERROR,
+        )
+
+    @pytest.mark.parametrize("raises", (True, False))
+    def test_task_comment_ok(self, raises):
         """It should update a task on Phabricator."""
-        self.phab.task_comment("T12345", "new message")
+        self.phab.task_comment("T12345", "new message", raises=raises)
         self.mocked_phabricator_client.maniphest.edit.assert_called_once_with(
             objectIdentifier="T12345", transactions=self.task_comment_transactions
         )
 
-    def test_task_comment_dry_run(self):
+    @pytest.mark.parametrize("raises", (True, False))
+    def test_task_comment_dry_run(self, raises):
         """It should not update a task on Phabricator when in DRY-RUN mode."""
         phab = phabricator.Phabricator(self.mocked_phabricator_client)
-        phab.task_comment("T12345", "new message")
+        phab.task_comment("T12345", "new message", raises=raises)
         assert not self.mocked_phabricator_client.maniphest.edit.called
 
-    def test_task_comment_fail(self):
+    @pytest.mark.parametrize("raises", (True, False))
+    def test_task_comment_empty_task(self, raises):
+        """It should not update a task on Phabricator if the task ID is empty and allow_empty_identifiers=True."""
+        self.phab_allow_empty.task_comment("", "new message", raises=raises)
+        assert not self.mocked_phabricator_client.maniphest.edit.called
+
+    @pytest.mark.parametrize("task_id", ("", "T12345"))
+    def test_task_comment_fail_raise(self, task_id):
         """It should raise PhabricatorError if the update operation fails."""
         self.mocked_phabricator_client.maniphest.edit.side_effect = RuntimeError
-        with pytest.raises(phabricator.PhabricatorError, match="Unable to update Phabricator task T12345"):
-            self.phab.task_comment("T12345", "new message")
+        with pytest.raises(phabricator.PhabricatorError, match=f"Unable to update Phabricator task '{task_id}'"):
+            self.phab.task_comment(task_id, "new message")
 
-        self.mocked_phabricator_client.maniphest.edit.assert_called_once_with(
-            objectIdentifier="T12345", transactions=self.task_comment_transactions
-        )
+        if task_id:
+            self.mocked_phabricator_client.maniphest.edit.assert_called_once_with(
+                objectIdentifier="T12345", transactions=self.task_comment_transactions
+            )
+        else:
+            self.mocked_phabricator_client.maniphest.edit.assert_not_called()
+
+    @pytest.mark.parametrize("task_id", ("", "T12345"))
+    def test_task_comment_fail_no_raise(self, task_id, caplog):
+        """It should not raise PhabricatorError but log a message if the update operation fails with raises=False."""
+        self.mocked_phabricator_client.maniphest.edit.side_effect = RuntimeError
+        with caplog.at_level(logging.ERROR):
+            self.phab.task_comment(task_id, "new message", raises=False)
+
+        check_logs(caplog, f"Unable to update Phabricator task '{task_id}' (raises=False)", logging.ERROR)
+        if task_id:
+            self.mocked_phabricator_client.maniphest.edit.assert_called_once_with(
+                objectIdentifier="T12345", transactions=self.task_comment_transactions
+            )
+        else:
+            self.mocked_phabricator_client.maniphest.edit.assert_not_called()
